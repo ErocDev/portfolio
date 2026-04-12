1f20a4db3e9d7660e68e6bec34c3c002663e33f0
Eric <182379722+ErocDev@users.noreply.github.com>
2026-04-12 21:35:44 +0100

    Fix audio

--- src/navigation.js
+++ src/navigation.js
@@ -7,12 +7,13 @@ import {
 } from "./transitions.js";
 import { isOnStartScreen } from "./main.js";
 import gsap from "gsap";
-import menuSrc from "./assets/menu.mp3";
+import enterSrc from "./assets/enter.mp3";
+import selectSrc from "./assets/select.mp3";
 
 const menuItems = document.querySelectorAll("#menu ul li");
-const selectNoise = new Audio("/src/assets/select.mp3");
+const selectNoise = new Audio(selectSrc);
 selectNoise.volume = 0.2;
-const enterNoise = new Audio("/src/assets/enter.mp3");
+const enterNoise = new Audio(enterSrc);
 enterNoise.volume = 0.2;
 let selectedIndex = 0;
 let animationsReady = false;
